9a6834410be3a7c4c40f1d326be3bebeec23d2d7
ssong0205 <caro0304@naver.com>
2017-06-21 17:50:46 +0900

    added output "What is Git?"

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello git");
+		System.out.println("Hello git!");
+		System.out.println("What is Git?");
 	}
-} 
+}
+
